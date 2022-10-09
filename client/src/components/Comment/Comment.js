cd41e935f0dbeea24fffb434ba255f47e64b8369
MisterTino <deezaymisstertino@gmail.com>
2022-10-10 04:50:30 +0700

    UPDATE: Comment post & get comment

--- client/src/components/Comment/Comment.js
+++ client/src/components/Comment/Comment.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { getUser } from '../../api/UserRequest'
 const Comment = ({ comment, comments }) => {
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER
   // State
   const [userComment, setUserComment] = useState({})
   // Get user
@@ -19,12 +20,26 @@ const Comment = ({ comment, comments }) => {
           to={`/profile/${userComment._id}`}
           style={{ textDecoration: 'none', color: 'black' }}
         >
+          <img
+            src={
+              userComment.profilePicture
+                ? serverPublic + userComment.profilePicture
+                : serverPublic + 'user.png'
+            }
+            alt=""
+            style={{
+              width: '1.5rem',
+              height: '1.5rem',
+              borderRadius: '50%',
+              marginRight: '5px',
+            }}
+          />
           <b>
             {userComment.firstname} {userComment.lastname}
           </b>
         </Link>
       </span>
-      <span style={{ margin: '0 15px 10px 15px' }}>{comment.text}</span>
+      <span style={{ margin: '0 10px 10px 20px' }}>{comment.text}</span>
     </>
   )
 }
