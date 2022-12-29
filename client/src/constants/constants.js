b22d894c76f15abe7a9c8354d7954de73d41c69d
MisterTino <deezaymisstertino@gmail.com>
2022-12-29 14:59:24 +0700

    local

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,4 +1,5 @@
 export const URL_SERVER = 'http://localhost:5000'
 export const URL_CLIENT = 'http://localhost:3000'
 
-export const PUBLIC_FOLDER = 'http://localhost:5000/images/'
+// export const URL_SERVER = 'https://tambv-social-server.herokuapp.com/'
+// export const PUBLIC_FOLDER = 'https://tambv-social-server.herokuapp.com/images/'
