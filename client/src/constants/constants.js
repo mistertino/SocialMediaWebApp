b1abf0e52550c0c436b6a96066a197a42236b8e5
MisterTino <deezaymisstertino@gmail.com>
2022-12-30 13:15:39 +0700

    no message

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,2 +1,2 @@
 export const URL_SERVER = 'https://tambv-social-server.herokuapp.com/'
-export const PUBLIC_FOLDER = 'https://tambv-social-server.herokuapp.com/images/'
+export const URL_CLIENT = 'https://tcmedia.netlify.app'
