ecf9b68b2e8e25dc6940c9dd445d0350e28dcd69
MisterTino <deezaymisstertino@gmail.com>
2023-01-08 12:02:28 +0700

    set URL

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,6 @@
-export const URL_SERVER = 'http://localhost:5000'
-export const URL_CLIENT = 'http://localhost:3000'
+// export const URL_SERVER = 'http://localhost:5000'
+// export const URL_CLIENT = 'http://localhost:3000'
 
-// export const URL_SERVER = 'https://tambv-social-server.herokuapp.com/'
+export const URL_SERVER = 'https://tambv-social-server.herokuapp.com/'
 // export const PUBLIC_FOLDER = 'https://tambv-social-server.herokuapp.com/images/'
+export const URL_CLIENT = 'https://tcmedia.netlify.app'
